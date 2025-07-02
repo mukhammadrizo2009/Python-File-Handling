202046b605054e18bb87b50da6d5571e939ec244
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2025-07-02 08:46:29 +0500

    Mission Done

--- task03.py
+++ task03.py
@@ -1,6 +1,9 @@
 with open("Input/numbers.txt", "r+") as file:
     result = file.readlines()
-    result_max = max(result ,key=lambda x: x)
+    
+    result1 = result.split()
+    
+    result_max = max(result1 , key=int)
     
 with open("Ouput/output03.txt","w") as file02:
     file02.write(str(result_max))
